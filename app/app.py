d91b9f89d63569dfeb0be8584619eb3a447524c3
Ramazan Erdem Uysal <erdemuysal@yahoo.com>
2021-05-13 19:43:41 +0300

    Unnecessary packages removed from app.py

--- app/app.py
+++ app/app.py
@@ -1,39 +1,29 @@
-from __future__ import division, print_function
-# coding=utf-8
-import sys
-import os
-import re
-import io
-import re
+#!/usr/bin/env python
 import numpy as np
-from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
+from flask import Flask, request, render_template
 from keras.models import load_model
 from keras.preprocessing.image import load_img, img_to_array, array_to_img
 from PIL import Image
 
 
-IMAGE_SIZE = (512, 512)
-MODEL_PATH = 'model/model.25-0.84.h5'
-mean = 51.43062
-std = 44.75102
-label_dict = {0:'Bone Fracture Negative', 1:'Bone Fracture Positive'}
+MODEL_PATH = './results/models/model.13-0.79.h5'
+IMAGE_SIZE = (224, 224)
+MEAN = 51.43062
+STD = 44.75102
+LABEL_DICT = {0:'Bone Fracture Negative', 1:'Bone Fracture Positive'}
 
 model = load_model(MODEL_PATH)
-
-
-app = Flask(__name__) 
+app = Flask(__name__)
 
 
 def preprocess(file_path):
 	img = load_img(file_path, color_mode='grayscale', target_size=IMAGE_SIZE)
 	img = img_to_array(img)
 	img = np.asarray(img).astype('float32')
-	img = np.squeeze(img, axis=-1) 
+	img = np.squeeze(img, axis=-1)
 	img = np.repeat(img[..., np.newaxis], 3, -1)
 	img = img[np.newaxis, ...]
-	img = (img - mean) / std
+	img = (img - MEAN) / STD
 
 	return img
 
@@ -63,7 +53,7 @@ def upload():
 	    # Make prediction
 	    y_pred = model.predict(img)
 	    label = decode_label(y_pred[0][0])
-	    result = label_dict[label]
+	    result = LABEL_DICT[label]
 
 	    return result
     return None
